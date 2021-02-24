51a69ae68f229417f34d3984968e73063a7034e6
coolone <coolone.official@gmail.com>
2021-02-24 22:23:46 +0300

    Refactoring and renaming

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,22 +8,24 @@ import VkontakterMiddleware
 import Botter
 
 extension Application {
-    static var databaseURL: URL = URL(string: Environment.get("DATABASE_URL")!)!
-    static var tgToken = Environment.get("TG_BOT_TOKEN")!
-    static var tgBufferUserId = Int64(Environment.get("TG_BUFFER_USER_ID")!)!
-    static var vkBufferUserId = Int64(Environment.get("VK_BUFFER_USER_ID")!)!
-    static var vkToken = Environment.get("VK_GROUP_TOKEN")!
-    static var vkGroupId: UInt64? = {
+    static let databaseURL: URL = URL(string: Environment.get("DATABASE_URL")!)!
+    static let tgToken = Environment.get("TG_BOT_TOKEN")!
+    static let tgBufferUserId = Int64(Environment.get("TG_BUFFER_USER_ID")!)!
+    static let vkBufferUserId = Int64(Environment.get("VK_BUFFER_USER_ID")!)!
+    static let vkAdminNickname: String = Environment.get("VK_ADMIN_NICKNAME")!
+    static let tgAdminNickname: String = Environment.get("TG_ADMIN_NICKNAME")!
+    static let vkToken = Environment.get("VK_GROUP_TOKEN")!
+    static let vkGroupId: UInt64? = {
         if let groupIdStr = Environment.get("VK_GROUP_ID"),
            let groupId = UInt64(groupIdStr) {
             return groupId
         }
         return nil
     }()
-    static var vkServerName: String? = Environment.get("VK_NEW_SERVER_NAME")
-    static var vkWebhooksUrl: String = Environment.get("WEBHOOKS_VK_URL")!
-    static var tgWebhooksUrl: String = Environment.get("WEBHOOKS_TG_URL")!
-    static var tgWebhooksPort: Int = Int(Environment.get("WEBHOOKS_TG_PORT")!)!
+    static let vkServerName: String? = Environment.get("VK_NEW_SERVER_NAME")
+    static let vkWebhooksUrl: String = Environment.get("WEBHOOKS_VK_URL")!
+    static let tgWebhooksUrl: String = Environment.get("WEBHOOKS_TG_URL")!
+    static let tgWebhooksPort: Int = Int(Environment.get("WEBHOOKS_TG_PORT")!)!
 }
 
 // configures your application
@@ -48,6 +50,8 @@ private func configurePostgres(_ app: Application) throws {
     app.migrations.add(CreateStylistPhotos())
     app.migrations.add(CreateMakeupers())
     app.migrations.add(CreateMakeuperPhotos())
+    app.migrations.add(CreateStudios())
+    app.migrations.add(CreateStudioPhotos())
     if app.environment == .development {
         try app.autoMigrate().wait()
     }
@@ -72,47 +76,57 @@ private func configurePostgres(_ app: Application) throws {
         let test = try PlatformFile(platform: [
             .tg("AgACAgQAAxkDAAIHjGAyWdeuTYimywkuaJsCk6cnPBw_AAKIqDEb84k9Ulm1-biSJET0czAfGwAEAQADAgADbQADE8MBAAEeBA"),
             .vk("photo-119092254_457239065")
-        ], type: .photo).toModel(app: app).wait()
+        ], type: .photo).saveModel(app: app).wait()
         
         for num in 1...20 {
             try Stylist(
                 name: "Stylist \(num)", photos: [try test.toMyType()]
-            ).toModel(app: app).wait()
+            ).saveModel(app: app).wait()
         }
         
         for num in 1...20 {
             try Makeuper(
                 name: "Makeuper \(num)", photos: [try test.toMyType()]
-            ).toModel(app: app).wait()
+            ).saveModel(app: app).wait()
+        }
+        
+        for num in 1...3 {
+            try Studio(
+                name: "Studio \(num)",
+                description: "Studio desc",
+                photos: [try test.toMyType()],
+                address: "adsdsad",
+                coords: .init(lat: 0, long: 0)
+            ).saveModel(app: app).wait()
         }
 
         let showcaseNodeId = try Node(
             name: "Showcase node",
             messagesGroup: [
                 .init(text: "Тут описание бота в деталях.", keyboard: [[
-                    .init(text: "Перейти в главное меню", action: .callback, eventPayload: .toNode(welcomeNodeId))
+                    .init(text: "Перейти в главное меню", action: .callback, eventPayload: .push(.entryPoint(.welcome)))
                 ]])
             ]
-        ).toModelWithId(app: app).wait()
+        ).saveModelReturningId(app: app).wait()
         
         try Node(
             name: "Welcome guest node",
             messagesGroup: [
                 .init(text: "Привет, $USER! Похоже ты тут впервые) Хочешь узнать что делает этот бот?", keyboard: [[
-                    .init(text: "Да", action: .callback, eventPayload: .toNode(showcaseNodeId)),
-                    .init(text: "Нет", action: .callback, eventPayload: .toNode(welcomeNodeId))
+                    .init(text: "Да", action: .callback, eventPayload: .push(.id(showcaseNodeId))),
+                    .init(text: "Нет", action: .callback, eventPayload: .push(.entryPoint(.welcome)))
                 ]])
             ],
-            //action: .init(.setName, success: .moveToNode(id: showcaseNodeId), failure: "Wrong name, please try again.")
+            //action: .init(.setName, success: .push(id: showcaseNodeId), failure: "Wrong name, please try again.")
             entryPoint: .welcomeGuest
-        ).toModelWithId(app: app).wait()
+        ).saveModelReturningId(app: app).wait()
         
         try Node(
             systemic: true,
-            name: "Изменить текст сообщения",
+            name: "Change static node text node",
             messagesGroup: [ .init(text: "Пришли мне новый текст") ],
             action: .init(.messageEdit, success: .pop)
-        ).toModelWithId(app: app).wait()
+        ).saveModelReturningId(app: app).wait()
         
     }
 }
@@ -124,7 +138,7 @@ func mainGroup(_ app: Application) throws -> UUID {
             .init(text: "Пришли мне прямую ссылку.")
         ],
         action: .init(.uploadPhoto)
-    ).toModelWithId(app: app).wait()
+    ).saveModelReturningId(app: app).wait()
     
     let aboutNodeId = try Node(
         name: "About node",
@@ -132,58 +146,73 @@ func mainGroup(_ app: Application) throws -> UUID {
             .init(text: "Test message here."),
             .init(text: "And other message.")
         ]
-    ).toModelWithId(app: app).wait()
+    ).saveModelReturningId(app: app).wait()
     
     let portfolioNodeId = try Node(
         name: "Portfolio node",
         messagesGroup: [
             .init(text: "Test message here.")
         ]
-    ).toModelWithId(app: app).wait()
+    ).saveModelReturningId(app: app).wait()
     
     let orderMainNodeId = try orderBuilderGroup(app)
-    
-//        try Node(
-//            systemic: true,
-//            name: "Create node",
-//            messagesGroup: .builder,
-//            action: .init(.createNode, success: .moveToBuilder(of: .node), failure: "Wrong text, please try again.")
-//        ).toModel().saveWithId(on: app.db).wait()
-    
+
     return try Node(
         name: "Welcome node",
         messagesGroup: [
             .init(text: "Добро пожаловать, $USER! Выбери секцию чтобы в нее перейти.", keyboard: [
                 [
-                    .init(text: "Обо мне", action: .callback, eventPayload: .toNode(aboutNodeId)),
-                    .init(text: "Мои работы", action: .callback, eventPayload: .toNode(portfolioNodeId)),
+                    .init(text: "Обо мне", action: .callback, eventPayload: .push(.id(aboutNodeId))),
+                    .init(text: "Мои работы", action: .callback, eventPayload: .push(.id(portfolioNodeId))),
                 ],
                 [
-                    .init(text: "Создание заявки", action: .callback, eventPayload: .toNode(orderMainNodeId)),
-                    .init(text: "Выгрузить фотку", action: .callback, eventPayload: .toNode(uploadPhotoNodeId))
+                    .init(text: "Заказ фотосессии", action: .callback, eventPayload: .push(.id(orderMainNodeId))),
+                    .init(text: "Выгрузить фотку", action: .callback, eventPayload: .push(.id(uploadPhotoNodeId)))
                 ]
             ])
         ],
         entryPoint: .welcome
-    ).toModelWithId(app: app).wait()
+    ).saveModelReturningId(app: app).wait()
 }
 
 func orderBuilderGroup(_ app: Application) throws -> UUID {
     let stylistNodeId = try Node(
         name: "Order builder stylist node",
-        messagesGroup: .list(.stylists)
-    ).toModelWithId(app: app).wait()
+        messagesGroup: .list(.stylists),
+        entryPoint: .orderBuilderStylist
+    ).saveModelReturningId(app: app).wait()
     
     let makeuperNodeId = try Node(
         name: "Order builder makeuper node",
-        messagesGroup: .list(.makeupers)
-    ).toModelWithId(app: app).wait()
+        messagesGroup: .list(.makeupers),
+        entryPoint: .orderBuilderMakeuper
+    ).saveModelReturningId(app: app).wait()
+    
+    let studioNodeId = try Node(
+        name: "Order builder studio node",
+        messagesGroup: .list(.studios),
+        entryPoint: .orderBuilderStudio
+    ).saveModelReturningId(app: app).wait()
+    
+    let finishNodeId = try Node(
+        name: "Order finish node",
+        messagesGroup: [
+            .init(text: "Заказ успешно создан, в ближайшее время с Вами свяжется @$ADMIN")
+        ],
+        entryPoint: .orderFinish
+    ).saveModelReturningId(app: app).wait()
+
+    try Node(
+        name: "Order checkout node",
+        messagesGroup: .orderCheckout,
+        entryPoint: .orderCheckout
+    ).saveModelReturningId(app: app).wait()
     
     return try Node(
         name: "Order builder main node",
-        messagesGroup: .orderBuilder(stylistNodeId, makeuperNodeId),
+        messagesGroup: .orderBuilder,
         entryPoint: .orderBuilder
-    ).toModelWithId(app: app).wait()
+    ).saveModelReturningId(app: app).wait()
 }
 
 func tgSettings(_ app: Application) -> Telegrammer.Bot.Settings {
